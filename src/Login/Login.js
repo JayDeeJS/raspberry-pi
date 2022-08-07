57903d836f816ed7bcb10c5164db6a0dfe5723ae
z to zee h <zhoomart.d@gmail.com>
2022-08-07 20:30:09 +0600

    fix: umbrella-corp login page logo auth fixed

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -30,7 +30,7 @@ const Login = () => {
     return (
         <div className={styles.login}>
             <Link to="/">
-                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/25d5851d-204c-4d9e-a61a-1895e0298a39/d31jfpe-07978a1f-4481-48b6-8323-d521e22801b5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI1ZDU4NTFkLTIwNGMtNGQ5ZS1hNjFhLTE4OTVlMDI5OGEzOVwvZDMxamZwZS0wNzk3OGExZi00NDgxLTQ4YjYtODMyMy1kNTIxZTIyODAxYjUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p1dgtByVIKmyCTNsDvZIYvIk3HyjL9cyHSIT8VjqY2Y" alt="home"/>
+                <img onClick={login} src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/25d5851d-204c-4d9e-a61a-1895e0298a39/d31jfpe-07978a1f-4481-48b6-8323-d521e22801b5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI1ZDU4NTFkLTIwNGMtNGQ5ZS1hNjFhLTE4OTVlMDI5OGEzOVwvZDMxamZwZS0wNzk3OGExZi00NDgxLTQ4YjYtODMyMy1kNTIxZTIyODAxYjUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p1dgtByVIKmyCTNsDvZIYvIk3HyjL9cyHSIT8VjqY2Y" alt="home"/>
             </Link>
             <div className={styles.loginContainer}>
                 <h1>Sign in</h1>
